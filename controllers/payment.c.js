eff0f77aad798c69b2f80af6952e7f7793789c4e
Thái Nguyễn Việt Hùng <hungthai10a1@gmail.com>
2024-12-23 00:12:32 +0700

    feat: implement authen

--- controllers/payment.c.js
+++ controllers/payment.c.js
@@ -1,9 +1,9 @@
 const model = require("../models/payment.m");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   getBalance: async (req, res) => {
     const account = await model.getByUserId(req.paymentAccount.id);
-
     let balance = 0;
 
     if (!account) {
@@ -29,4 +29,45 @@ module.exports = {
     const result2 = await model.updateBalance(-1, amount);
     res.sendStatus(200);
   },
+  authenticateToken: (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.paymentAccount = user;
+      next();
+    });
+  },
+
+  getAccessToken: async (req, res) => {
+    const Id = req.body.Id;
+    const user = { id: Id };
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    model.insertRefreshToken(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  },
+
+  getNewAccessToken: (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    if (refreshToken === null) return res.sendStatus(401);
+    if (!model.checkRefreshToken(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      const accessToken = generateAccessToken({ id: user.id });
+      res.json({ accessToken: accessToken });
+    });
+  },
+
+  deleteRefreshToken: async (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    await model.deleteRefreshToken(refreshToken);
+    res.sendStatus(204);
+  },
 };
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+}
