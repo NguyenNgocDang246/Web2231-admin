371855d86929de2760480d544be4ce17b651e74c
22120278 <nvhphuc364@gmail.com>
2024-12-23 23:49:34 +0700

    update discount model

--- models/discount.m.js
+++ models/discount.m.js
@@ -62,7 +62,9 @@ module.exports = {
   },
   one: async (id) => {
     try {
-      return Discount.findById(id).lean();
+      const rs = Discount.findById(id).lean();
+      rs.expiredAt = rs.expiredAt.toISOString().split("T")[0];
+      return rs;
     } catch (error) {
       throw error;
     }
