310d6f7aabe9f31da2d2136db2a1ca0ec35858db
NgocDang <ngocdang240604@gmail.com>
2025-01-01 18:55:58 +0700

    test process.env.NODE_ENV

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ connectDB()
                 },
             })
         );
+        console.log(process.env.NODE_ENV, process.env.NODE_ENV === "production");
         app.use(passport.initialize());
         app.use(passport.session());
 
