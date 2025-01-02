74248d29c29ea6087dcb8db3976069a3d1d1debb
Hoang Phuc <159674910+hoangphuc3604@users.noreply.github.com>
2025-01-02 08:24:15 +0700

    Revenue (#11)
    * feat: init structure for payment
    
    * feat: implement transfer money
    
    * feat: implement authen
    
    * feat: add routers for payment
    
    * connect to user Server
    
    * fix bug when do not found user
    
    * add https
    
    * save session in mongo db
    
    * remove https
    
    * test process.env.NODE_ENV
    
    * change session.secure = false
    
    * add app.set(trust proxy,1)
    
    * refactor: standardize quotes in server.js and improve code formatting
    
    ---------
    
    Co-authored-by: Thái Nguyễn Việt Hùng <hungthai10a1@gmail.com>
    Co-authored-by: Thái Nguyễn Việt Hùng <78160300+convitnhodev@users.noreply.github.com>
    Co-authored-by: NgocDang <ngocdang240604@gmail.com>
    Co-authored-by: NguyenNgocDang246 <110982997+NguyenNgocDang246@users.noreply.github.com>

---
+++ models/payment.m.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+
+// Định nghĩa schema cho User
+const bankAccountSchema = new mongoose.Schema({
+  id_user: { type: String, required: true, unique: true },
+  balance: { type: Number },
+});
+
+// Tạo model từ schema
+const BankAccount = mongoose.model(
+  "BankAccount",
+  bankAccountSchema,
+  "bankAccounts"
+);
+
+let initalized = false;
+async function createShopAccount() {
+    if(initalized){
+        return;
+    }
+    initalized = true;
+    const existAccount = await BankAccount.findOne({ id_user: "-1" });
+    if(existAccount){
+        return;
+    }
+    const newAccount = {
+        id_user: "-1",
+        balance: 0,
+    };
+    BankAccount.create(newAccount);
+}
+
+async function ensureInitalized() {
+    if(initalized) return;
+    await createShopAccount();
+}
+
+module.exports = {
+  all: async (page = 1, accountPerPage = null) => {
+    await ensureInitalized();
+    try {
+      const skip = (page - 1) * accountPerPage;
+      if (accountPerPage) {
+        const account = await BankAccount.find()
+          .skip(skip)
+          .limit(accountPerPage)
+          .lean();
+        return account;
+      } else {
+        const account = await BankAccount.find().skip(skip).lean();
+        return account;
+      }
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+  one: async (id) => {
+    await ensureInitalized();
+    try {
+      const account = await BankAccount.findById(id).lean();
+      return account;
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+  getByUserId: async (userId) => {
+    await ensureInitalized();
+    try {
+      const accounts = await BankAccount.findOne({ id_user: userId }).lean();
+      return accounts;
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+
+  updateBalance: async (userId, newBalance) => {
+    await ensureInitalized();
+    try {
+      const updatedAccount = await BankAccount.findOneAndUpdate(
+        { id_user: userId },
+        { $set: { balance: newBalance } },
+        { new: true, lean: true }
+      );
+      return updatedAccount;
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+  insert: async (account) => {
+    await ensureInitalized();
+    try {
+      const newAccount = await BankAccount.create(account);
+      return newAccount;
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  },
+};
--- server.js
+++ server.js
@@ -6,106 +6,125 @@ const session = require("express-session");
 const passport = require("passport");
 const flash = require("connect-flash");
 const path = require("path");
+const MongoStore = require("connect-mongo");
 const cookieParser = require("cookie-parser");
 
+const { mongoose, connectDB } = require("./configs/db/db");
 const initializePassport = require("./configs/passport");
 initializePassport(passport);
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.set("trust proxy", 1); // Bật trust proxy
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(flash());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 1 * 24 * 60 * 60 * 1000,
-    },
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Middleware for 404
-app.use((err, req, res, next) => {
-  if (app.get("env") === "development") {
-    res.status(err.status || 500).render("error", {
-      title: "Lỗi",
-      message: err.message,
-      error: err,
-    });
-  } else {
-    res.status(err.status || 500).render("error", {
-      title: "Lỗi",
-      message: "Đã xảy ra lỗi, vui lòng thử lại sau!",
-      error: {},
-    });
-  }
-});
-
-const hbs = create({
-  extname: ".hbs",
-  encoding: "utf-8",
-  layoutsDir: "./views/layouts",
-  partialsDir: "./views/partials",
-  defaultLayout: "main",
-  helpers: {
-    index: (array, index) => {
-      if (Array.isArray(array) && array.length > 0) {
-        return array[index];
-      }
-      return null;
-    },
-    inc: (value) => parseInt(value) + 1,
-    json: (context) => JSON.stringify(context),
-    eq: (v1, v2) => v1 === v2,
-    dateFormat: (date) => {
-      return new Date(date).toLocaleString();
-    },
-    neq: (v1, v2, options) => {
-      if (Number(v1) !== Number(v2)) {
-        return options.fn(this);
-      }
-      return options.inverse(this);
-    },
-  },
-});
-
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "./views");
-
-// Routes
-app.use("/", require("./routes/home"));
-app.use("/product", require("./routes/product.r"));
-app.use("/auth", require("./routes/auth.r"));
-app.use("/order", require("./routes/order.r"));
-app.use("/category", require("./routes/category.r"));
-app.use("/discount", require("./routes/discount.r"));
-app.use("/brand", require("./routes/brand.r"));
-app.use("/user", require("./routes/user.r"));
-app.use("/search", require("./routes/search.r"));
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "img-src 'self' data:; default-src 'self';"
-  );
-  next();
-});
-
-const { connectDB } = require("./configs/db/db");
+// Kết nối tới MongoDB và khởi tạo session
 connectDB()
   .then(() => {
+    const sessionDbConnection = mongoose.createConnection(
+      process.env.MONGODB_URI_SESSION
+    );
+
+    app.use(
+      session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+          client: sessionDbConnection.getClient(), // Sử dụng connection cho session
+          collectionName: "sessions", // Tên collection để lưu session
+        }),
+        cookie: {
+          secure: process.env.NODE_ENV === "production",
+          maxAge: 1 * 24 * 60 * 60 * 1000,
+        },
+      })
+    );
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // Đặt cấu hình Handlebars
+    const hbs = create({
+      extname: ".hbs",
+      encoding: "utf-8",
+      layoutsDir: "./views/layouts",
+      partialsDir: "./views/partials",
+      defaultLayout: "main",
+      helpers: {
+        index: (array, index) => {
+          if (Array.isArray(array) && array.length > 0) {
+            return array[index];
+          }
+          return null;
+        },
+        inc: (value) => parseInt(value) + 1,
+        json: (context) => JSON.stringify(context),
+        eq: (v1, v2) => v1 === v2,
+        dateFormat: (date) => {
+          return new Date(date).toLocaleString();
+        },
+        neq: (v1, v2, options) => {
+          if (Number(v1) !== Number(v2)) {
+            return options.fn(this);
+          }
+          return options.inverse(this);
+        },
+      },
+    });
+
+    // Đặt engine cho Handlebars
+    app.engine("hbs", hbs.engine);
+    app.set("view engine", "hbs");
+    app.set("views", "./views");
+
+    // Các route
+    app.use("/", require("./routes/home"));
+    app.use("/product", require("./routes/product.r"));
+    app.use("/auth", require("./routes/auth.r"));
+    app.use("/order", require("./routes/order.r"));
+    app.use("/category", require("./routes/category.r"));
+    app.use("/discount", require("./routes/discount.r"));
+    app.use("/brand", require("./routes/brand.r"));
+    app.use("/user", require("./routes/user.r"));
+    app.use("/search", require("./routes/search.r"));
+    app.use("/payment", require("./routes/payment.r"));
+    app.use("/revenue", require("./routes/revenue.r"));
+
+    // Middleware cho 404 và lỗi
+    app.use((err, req, res, next) => {
+      if (app.get("env") === "development") {
+        res.status(err.status || 500).render("error", {
+          title: "Lỗi",
+          message: err.message,
+          error: err,
+        });
+      } else {
+        res.status(err.status || 500).render("error", {
+          title: "Lỗi",
+          message: "Đã xảy ra lỗi, vui lòng thử lại sau!",
+          error: {},
+        });
+      }
+    });
+
+    // Cấu hình bảo mật Content-Security-Policy
+    app.use((req, res, next) => {
+      res.setHeader(
+        "Content-Security-Policy",
+        "img-src 'self' data:; default-src 'self';"
+      );
+      next();
+    });
+
+    // Khởi động server
     app.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
