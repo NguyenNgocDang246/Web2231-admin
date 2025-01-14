1ace09771bd90dfb2feba4780bbbdfd83a32e39d
22120278 <nvhphuc364@gmail.com>
2025-01-14 07:25:44 +0700

    refactor: standardize code formatting in database connection file

--- configs/db/db.js
+++ configs/db/db.js
@@ -1,18 +1,16 @@
-require('dotenv').config();  // Đảm bảo bạn đã tải môi trường từ .env
-const mongoose = require('mongoose');  // Import mongoose
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-// Lấy URI từ biến môi trường
 const mongoURI = process.env.DB_URI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log('Connected to MongoDB Atlas via Mongoose');
-    } catch (error) {
-        console.error('Error connecting to MongoDB Atlas:', error);
-        process.exit(1);  // Dừng ứng dụng nếu kết nối thất bại
-    }
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB Atlas via Mongoose");
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error);
+    process.exit(1);
+  }
 };
 
-// Export mongoose và connectDB
 module.exports = { mongoose, connectDB };
